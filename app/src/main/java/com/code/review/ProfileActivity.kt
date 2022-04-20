c86ccd48e584fd7dcc1ebc21cda5c429e4eecf17
Artur Vasilov <avasilov@yandex-team.ru>
2022-04-20 15:54:02 +0300

    Test PR commit

--- app/src/main/java/com/code/review/ProfileActivity.kt
+++ app/src/main/java/com/code/review/ProfileActivity.kt
@@ -1,14 +1,11 @@
 package com.code.review
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, ProfileActivity::class.java))
+        setContentView(R.layout.activity_profile)
     }
 }
